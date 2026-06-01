00c8b5daadf27fe679b2ff475a543c2f78b55fb1
Nuuvsen <nuuvsen@gmail.com>
2026-06-01 00:03:28 -0300

    Atualizando projeto

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'icon-192x192.png', 'icon-512x512.png'],
+      manifest: false, // Diz ao plugin para usar o seu manifest.json existente na pasta public
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true // Permite que você teste a instalação rodando 'npm run dev'
+      }
+    })
   ],
 })
